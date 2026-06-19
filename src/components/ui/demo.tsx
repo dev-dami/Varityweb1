62b3674062d80e145793a1bb7bb73c92e628516b
dev-dami <dev@example.com>
2026-06-19 21:20:21 +0100

    Fix SidebarDemo import of AppSidebar

--- src/components/ui/demo.tsx
+++ src/components/ui/demo.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { SessionNavBar } from "@/components/ui/sidebar"
+import AppSidebar from "../app/components/dashboard/sidebar"
 
 export function SidebarDemo() {
   return (
     <div className="flex h-screen w-screen flex-row">
-      <SessionNavBar />
+      <AppSidebar />
       <main className="flex h-screen grow flex-col overflow-auto">
       </main>
     </div>
